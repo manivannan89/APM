119fcdadc0bb7d7af93f550072b7b9ab040eb6d5
Manivannan Ponnusamy <mani.dx@gmail.com>
2017-08-26 22:52:01 -0700

    ngModel and dependency

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ProductsComponent } from './products/products.component';
@@ -13,6 +14,7 @@ import { AlertModule } from 'ngx-bootstrap';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AlertModule.forRoot()
   ],
   providers: [],
