08444d3a9b24fd5235d7e39d1c9148baeaf74bb6
Manivannan Ponnusamy <mani.dx@gmail.com>
2017-12-30 13:11:22 -0800

    Updated from double quotes to single quotes for json data. Since error thrown by jslint.

--- src/app/report/report.component.ts
+++ src/app/report/report.component.ts
@@ -17,35 +17,35 @@ export class ReportComponent implements OnInit {
 
   constructor() {
     this.dataSource = {
-      "chart": {
-        "caption": "Harry's SuperMart",
-        "subCaption": "Top 5 stores in last month by revenue",
-        "numberprefix": "$",
-        "theme": "fint"
+      'chart': {
+        'caption': 'Harry\'s SuperMart',
+        'subCaption': 'Top 5 stores in last month by revenue',
+        'numberprefix': '$',
+        'theme': 'fint'
       },
-      "data": [
+      'data': [
         {
-          "label": "Bakersfield Central",
-          "value": "880000"
+          'label': 'Bakersfield Central',
+          'value': '880000'
         },
         {
-          "label": "Garden Groove harbour",
-          "value": "730000"
+          'label': 'Garden Groove harbour',
+          'value': '730000'
         },
         {
-          "label": "Los Angeles Topanga",
-          "value": "590000"
+          'label': 'Los Angeles Topanga',
+          'value': '590000'
         },
         {
-          "label": "Compton-Rancho Dom",
-          "value": "520000"
+          'label': 'Compton-Rancho Dom',
+          'value': '520000'
         },
         {
-          "label": "Daly City Serramonte",
-          "value": "330000"
+          'label': 'Daly City Serramonte',
+          'value': '330000'
         }
       ]
-    }
+    };
   }
 
   ngOnInit() {
